25b7cf781bc42f2e726f9af06c81bc22765dbee7
Matt Hancock <mhancock743@gmail.com>
2018-05-22 10:21:15 -0500

    subset selection option for splitter utility

--- stat_learn_level_set/utils/data/splitter.py
+++ stat_learn_level_set/utils/data/splitter.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def split(keys, p=[0.6, 0.2, 0.2], rs=None):
+def split(keys, p=[0.6, 0.2, 0.2], subset_size=None, rs=None):
     """
     keys: list of strings
         List of keys to split into training, validation, and testing.
@@ -8,15 +8,27 @@ def split(keys, p=[0.6, 0.2, 0.2], rs=None):
     p: list of 3 fractions.
         The probability of being placed in the training, validation or testing.
 
+    subset_size: int, default=None
+        If supplied, then should be less than or equal to `len(keys)`. If 
+        given, then `keys` is first subsampled by `subset_size` before 
+        splitting.
+
     rs: numpy.random.RandomState
         For reproducible results.
     """
+    if subset_size is not None and subset_size > len(keys):
+        raise ValueError("`subset_size` must be <= `len(keys)`.")
+    if subset_size is None:
+        subset_size = len(keys)
+
     rs = np.random.RandomState() if rs is None else rs
-    mn = rs.multinomial(1, pvals=p, size=len(keys))
+
+    subkeys = rs.choice(keys, replace=False, size=subset_size)
+    mn = rs.multinomial(1, pvals=p, size=len(subkeys))
 
     datasets = {}
 
     for j,d in enumerate(['tr', 'va', 'ts']):
-        datasets[d] = [keys[i] for i in range(len(keys)) if mn[i,j]==1]
+        datasets[d] = [subkeys[i] for i in range(len(subkeys)) if mn[i,j]==1]
         
     return datasets
