6e84bd7177b546024b795933c10f01b549f6be96
matt <mhancock743@gmail.com>
2018-12-19 21:32:17 -0600

    Continue feature refactor

--- level_set_machine_learning/feature_map/features/base_feature.py
+++ level_set_machine_learning/feature_map/features/base_feature.py
@@ -1,13 +1,18 @@
+""" Abstract base classes for features
+"""
 import abc
 import numpy
 
-LOCAL_IMAGE_FEATURE_TYPE = 'local-image'
-LOCAL_SHAPE_FEATURE_TYPE = 'local-shape'
-GLOBAL_IMAGE_FEATURE_TYPE = 'global-image'
-GLOBAL_SHAPE_FEATURE_TYPE = 'global-shape'
+
+IMAGE_FEATURE_TYPE = 'image'
+SHAPE_FEATURE_TYPE = 'shape'
+LOCAL_FEATURE_TYPE = 'local'
+GLOBAL_FEATURE_TYPE = 'global'
 
 
 class BaseFeature(abc.ABC):
+    """ The abstract base class for all features
+    """
 
     @property
     @abc.abstractmethod
@@ -19,7 +24,15 @@ class BaseFeature(abc.ABC):
     @property
     @abc.abstractmethod
     def type(self):
-        """ The feature type: e.g., 'local-image', 'global-shape', etc """
+        """ The type of the feature, e.g., image or shape
+        """
+        raise NotImplementedError
+
+    @property
+    @abc.abstractmethod
+    def locality(self):
+        """ The locality of the feature, e.g., local versus global
+        """
         raise NotImplementedError
 
     def __init__(self, ndim):
@@ -40,12 +53,12 @@ class BaseFeature(abc.ABC):
 
         # Check delta terms
         if dx is None:
-            dx = numpy.ones(img.ndim)
+            dx = numpy.ones(self.ndim)
         else:
             dx = numpy.array(dx)
-            if len(dx) != img.ndim:
+            if len(dx) != self.ndim:
                 msg = "Number of dx terms ({}) doesn't match dimensions ({})"
-                raise ValueError(msg.format(len(dx), img.ndim))
+                raise ValueError(msg.format(len(dx), self.ndim))
 
         # Check dtype of mask
         if mask.dtype != numpy.bool:
@@ -55,7 +68,7 @@ class BaseFeature(abc.ABC):
         return self.compute_feature(u=u, img=img, dist=dist, mask=mask, dx=dx)
 
     @abc.abstractmethod
-    def compute_feature(self, u, img, dist, mask, dx=None):
+    def compute_feature(self, u, img, dist, mask, dx):
         """ Compute the feature
 
         Parameters
@@ -73,12 +86,79 @@ class BaseFeature(abc.ABC):
         mask: numpy.array (dtype=bool)
             The boolean mask indicating the narrow band region of `u`.
 
-        dx: numpy.array, shape=img.ndim
+        dx: numpy.array, shape=ndim
             The "delta" spacing terms for each image axis. If None, then
             1.0 is used for each axis.
 
         Returns
         -------
-        feature: numpy.array, shape=img.shape
+        feature: numpy.array, shape=u.shape
+        """
+        raise NotImplementedError
+
+
+class BaseImageFeature(BaseFeature):
+    """ The abstract base class for all image features
+    """
+    type = 'image'
+
+
+class BaseShapeFeature(BaseFeature):
+    """ The abstract base class for all shape features
+    """
+    type = 'shape'
+
+    def __call__(self, u, dist, mask, dx=None):
+        """ Calls the feature computation function after performing
+        some validation on the inputs
+        """
+
+        # Check shapes
+        shape = u.shape
+        if dist.shape != shape or mask.shape != shape:
+            shapes = (u.shape, dist.shape, mask.shape)
+            msg = ("Shape mismatch in one of the inputs: u={}, "
+                   "dist={}, mask={}")
+            raise ValueError(msg.format(*shapes))
+
+        # Check delta terms
+        if dx is None:
+            dx = numpy.ones(self.ndim)
+        else:
+            dx = numpy.array(dx)
+            if len(dx) != self.ndim:
+                msg = "Number of dx terms ({}) doesn't match dimensions ({})"
+                raise ValueError(msg.format(len(dx), self.ndim))
+
+        # Check dtype of mask
+        if mask.dtype != numpy.bool:
+            msg = "mask dtype ({}) was not of type bool"
+            raise ValueError(msg.format(mask.dtype))
+
+        return self.compute_feature(u=u, dist=dist, mask=mask, dx=dx)
+
+    @abc.abstractmethod
+    def compute_feature(self, u, dist, mask, dx):
+        """ Compute the feature
+
+        Parameters
+        ----------
+        u: numpy.array
+            The "level set function".
+
+        dist: numpy.array
+            The signed distance transform to the level set `u` (only computed
+            necessarily in the narrow band region).
+
+        mask: numpy.array (dtype=bool)
+            The boolean mask indicating the narrow band region of `u`.
+
+        dx: numpy.array, shape=ndim
+            The "delta" spacing terms for each image axis. If None, then
+            1.0 is used for each axis.
+
+        Returns
+        -------
+        feature: numpy.array, shape=u.shape
         """
         raise NotImplementedError
--- level_set_machine_learning/feature_map/features/shape.py
+++ level_set_machine_learning/feature_map/features/shape.py
@@ -1,15 +1,15 @@
 import numpy
 
 from .base_feature import (
-    BaseFeature, LOCAL_SHAPE_FEATURE_TYPE, GLOBAL_SHAPE_FEATURE_TYPE)
+    BaseShapeFeature, LOCAL_FEATURE_TYPE, GLOBAL_FEATURE_TYPE)
 
 
-class Size(BaseFeature):
-    """ Yields the size of the region enclosed by the zero level set of u.
+class Size(BaseShapeFeature):
+    """ Computes the size of the region enclosed by the zero level set of u.
     In 1D, this is length. In 2D, it is area, and in 3D, it is volume.
     """
 
-    type = GLOBAL_SHAPE_FEATURE_TYPE
+    locality = GLOBAL_FEATURE_TYPE
 
     @property
     def name(self):
@@ -22,7 +22,7 @@ class Size(BaseFeature):
         else:
             return 'hyper-volume'
 
-    def compute_feature(self, u, img, dist, mask, dx):
+    def compute_feature(self, u, dist, mask, dx):
 
         size = (u > 0).sum() * numpy.prod(dx)
         feature = numpy.empty(u.shape)
@@ -31,34 +31,66 @@ class Size(BaseFeature):
         return feature
 
 
-class Boundary(BaseFeature):
+class BoundarySize(BaseShapeFeature):
     """ Computes the size of the zero-level set of u. In 2D, this is
     the arc-length of the implicit curve. In 3D, it is surface area.
+
+    This uses a volume integral:
+
+        :math:`\\int\\int \\delta(u) \\| Du \\| dx dy`
+
+    to compute the length of the boundary contour via Co-Area formula.
     """
-    type = GLOBAL_SHAPE_FEATURE_TYPE
+
+    locality = GLOBAL_FEATURE_TYPE
 
     @property
     def name(self):
         if self.ndim == 1:
-            return 'length'
+            return 'zeros'
         elif self.ndim == 2:
-            return 'area'
+            return 'arc-length'
         elif self.ndim == 3:
-            return 'volume'
+            return 'surface-area'
         else:
-            return 'hyper-volume'
+            return 'hyper-surface-area'
 
-    def compute_feature(self, u, img, dist, mask, dx):
+    def compute_feature(self, u, dist, mask, dx):
 
-        H = (u > 0).astype(numpy.float)
+        pos_part = (u > 0).astype(numpy.float)
 
-        gradient = numpy.gradient(H.astype(numpy.float), *dx)
+        gradient = numpy.gradient(pos_part.astype(numpy.float), *dx)
         gradient_magnitude = numpy.zeros_like(gradient[0])
-        gmagH = [grad**2]
 
-        # Boundary length
-        # This uses a volume integral:
-        # :math:`\\int\\int \\delta(u) \\| Du \\| dx dy`
-        # to compute the length of the boundary contour via Co-Area formula.
-        L = gmagH.sum() * pdx
-        F[mask,1] = L
+        for grad in gradient:
+            gradient_magnitude += grad**2
+        gradient_magnitude **= 0.5
+
+        return gradient_magnitude.sum() * numpy.prod(dx)
+
+
+class IsoperimetricRatio(BaseShapeFeature):
+    """ Computes the isoperimetric ratio, which is a measure of
+    circularity in two dimensions and a measure of sphericity in three.
+    """
+
+    locality = GLOBAL_FEATURE_TYPE
+
+    @property
+    def name(self):
+        if self.ndim == 2:
+            return 'circularity'
+        else:
+            return 'sphericity'
+
+    def __init__(self, ndim):
+        if ndim < 2 or ndim > 3:
+            msg = ("Isoperimetric ratio defined for dimensions 2 and 3; "
+                   "ndim supplied = {}")
+            raise ValueError(msg.format(ndim))
+
+        super(IsoperimetricRatio, self).__init__(ndim)
+
+    def compute_feature(self, u, dist, mask, dx):
+
+        return gradient_magnitude.sum() * numpy.prod(dx)
