c92ead8229670c8e4d4add3166674d6b277548f4
matt <mhancock743@gmail.com>
2018-12-28 09:37:31 -0600

    Add image edge feature and unit tests

--- level_set_machine_learning/feature/image.py
+++ level_set_machine_learning/feature/image.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 import numpy
 from scipy.ndimage.filters import gaussian_filter1d
 
@@ -5,13 +7,13 @@ from .base_feature import (
     BaseImageFeature, LOCAL_FEATURE_TYPE, GLOBAL_FEATURE_TYPE)
 
 
-class SmoothedImageSample(BaseImageFeature):
+class ImageSample(BaseImageFeature):
     """ The gaussian-smoothed image sampled locally
     """
     locality = LOCAL_FEATURE_TYPE
 
     def name(self):
-        return "Smoothed image sample (sigma = {.3f})".format(self.sigma)
+        return "Image sample (sigma = {.3f})".format(self.sigma)
 
     def __init__(self, ndim, sigma):
         """ Initialize a smoothed image sample feature
@@ -26,7 +28,7 @@ class SmoothedImageSample(BaseImageFeature):
             provided dx terms)
 
         """
-        super(SmoothedImageSample, self).__init__(ndim)
+        super(ImageSample, self).__init__(ndim)
         self.sigma = sigma
 
     def compute_feature(self, u, img, dist, mask, dx):
@@ -42,6 +44,54 @@ class SmoothedImageSample(BaseImageFeature):
                     smoothed, sigma=self.sigma / dx[i], axis=i)
 
             feature[mask] = smoothed[mask]
+
+        return feature
+
+
+class ImageEdgeSample(BaseImageFeature):
+    """ The gaussian-smoothed image edge (gradient magnitude) sampled locally
+    """
+    locality = LOCAL_FEATURE_TYPE
+
+    def name(self):
+        return "Image edge sample (sigma = {.3f})".format(self.sigma)
+
+    def __init__(self, ndim, sigma):
+        """ Initialize a smoothed image sample feature
+
+        ndim: int
+            The number of dimensions in which the feature will be computed
+
+        sigma: float
+            The smooth parameter for Gaussian smoothing (note that
+            sigma = 0 yields no smoothing; also note that anisotropic
+            volumes will alter sigma along each axis according to the
+            provided dx terms)
+
+        """
+        super(ImageEdgeSample, self).__init__(ndim)
+        self.sigma = sigma
+
+    def compute_feature(self, u, img, dist, mask, dx):
+        feature = numpy.empty_like(u)
+
+        if self.sigma == 0:
+            gradients = numpy.gradient(img, *dx)
+
+        else:
+            gradients = [
+                gaussian_filter1d(
+                    img, sigma=self.sigma/dx[axis], order=1, axis=axis)
+                for axis in range(self.ndim)
+            ]
+
+        # Square, sum, and square-root the gradient terms to form the magnitude
+        gradient_magnitude = reduce(lambda a, b: a+b**2,
+                                    gradients,
+                                    numpy.zeros_like(img))**0.5
+
+        # Place the gradient magnitude into the feature array
+        feature[mask] = gradient_magnitude[mask]
 
         return feature
 
