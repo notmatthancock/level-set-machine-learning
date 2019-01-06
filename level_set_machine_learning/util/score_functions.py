a37e30ac66b0ee0566d1fefdea388bb4b772394b
matt <mhancock743@gmail.com>
2019-01-05 20:24:10 -0600

    Add dice coef to score functions

--- level_set_machine_learning/util/score_functions.py
+++ level_set_machine_learning/util/score_functions.py
@@ -1,19 +1,30 @@
-import numpy as np
+import numpy
 
 
-def jaccard(u, seg, t=0.0):
-    """ Compute the Jaccard overlap score between `u > t` and `seg`. Also
-    known as the intersection over union.
+def jaccard(u, seg, threshold=0.0):
+    """ Compute the Jaccard overlap score between `u > threshold` and `seg`
+    (Also known as the "intersection over union")
     """
-    if seg.dtype != np.bool:
+    if seg.dtype != numpy.bool:
         msg = "`seg` dtype ({}) was not of type bool"
         raise ValueError(msg.format(seg.dtype))
 
-    thresholded = u > t
-    intersection = (thresholded & seg).sum() * 1.0
-    union = (thresholded | seg).sum() * 1.0
+    thresholded = u > threshold
 
-    if intersection == 0:
+    intersection = float((thresholded & seg).sum())
+    union = float((thresholded | seg).sum())
+
+    if union == 0:
+        # If union is zero then both `seg` and `u > threshold` are empty! This
+        # is an edge case, but we'll say that the "overlap" score is perfect,
+        # rather than nil.
         return 1.0
     else:
         return intersection / union
+
+
+def dice(u, seg, threshold=0.0):
+    """ Compute the Dice coefficient between `u > threshold` and `seg`
+    """
+    jaccard_score = jaccard(u, seg, threshold)
+    return 2.0 * jaccard_score / (jaccard_score + 1.0)
